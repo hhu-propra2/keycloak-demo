b172839a48e66deb95e64a92c6358ec70af4279c
Jens Bendisposto <jens@bendisposto.de>
2020-03-01 11:24:30 +0100

    custom metrics for Prometheus

--- src/main/java/mops/authdemo/AuthdemoController.java
+++ src/main/java/mops/authdemo/AuthdemoController.java
@@ -1,5 +1,7 @@
 package mops.authdemo;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
@@ -12,12 +14,24 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthdemoController {
+
+    private final Counter authenticatedAccess;
+    private final Counter publicAccess;
+
+    public AuthdemoController(MeterRegistry registry) {
+        authenticatedAccess = registry.counter("access.authenticated");
+        publicAccess = registry.counter("access.public");
+    }
+
+
     /**
      * Nimmt das Authentifizierungstoken von Keycloak und erzeugt ein AccountDTO für die Views.
      *
      * @param token
      * @return neuen Account der im Template verwendet wird
      */
+
+
     private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
         KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
         return new Account(
@@ -32,6 +46,7 @@ public class AuthdemoController {
         if (token != null) {
             model.addAttribute("account", createAccountFromPrincipal(token));
         }
+        publicAccess.increment();
         return "index";
     }
 
@@ -40,6 +55,7 @@ public class AuthdemoController {
     public String orga(KeycloakAuthenticationToken token, Model model) {
         model.addAttribute("account", createAccountFromPrincipal(token));
         model.addAttribute("entries", Entry.generate(10));
+        authenticatedAccess.increment();
         return "orga";
     }
 
@@ -48,6 +64,7 @@ public class AuthdemoController {
     public String studentin(KeycloakAuthenticationToken token, Model model) {
         model.addAttribute("account", createAccountFromPrincipal(token));
         model.addAttribute("entries", Entry.generate(10));
+        authenticatedAccess.increment();
         return "studentin";
     }
 
@@ -56,6 +73,7 @@ public class AuthdemoController {
     @RolesAllowed({"ROLE_orga", "ROLE_studentin"})
     public String personal(KeycloakAuthenticationToken token, Model model) {
         model.addAttribute("account", createAccountFromPrincipal(token));
+        authenticatedAccess.increment();
         return "personal";
     }
 
